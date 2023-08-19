1db15b0901f81b43f4697a6342a65385755a8130
Jose Luiz <luizbsilva@gmail.com>
2023-08-19 10:53:48 -0300

    Recebendo e tratando mensagens com RabbitMQ

--- compra/src/main/java/com/mscompra/service/rabbitmq/Producer.java
+++ compra/src/main/java/com/mscompra/service/rabbitmq/Producer.java
@@ -1,24 +1,27 @@
 package com.mscompra.service.rabbitmq;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mscompra.model.Pedido;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Service
 public class Producer {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
+    private final Queue queue;
+    private final ObjectMapper mapper;
 
-    @Autowired
-    private Queue queue;
-
+    @SneakyThrows
     @PostMapping
     public void enviarPedido(Pedido pedido) {
-        rabbitTemplate.convertAndSend(queue.getName(), pedido);
+        rabbitTemplate.convertAndSend(queue.getName(), mapper.writeValueAsString(pedido));
     }
 
 }
